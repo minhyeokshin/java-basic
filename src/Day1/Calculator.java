c20f08e9381c5cc069e521ac4ced603de894e650
minhyeokshin <sinminheyok@gmail.com>
2025-01-08 19:03:24 +0900

    Day1

--- src/Day1/Calculator.java
+++ src/Day1/Calculator.java
@@ -2,6 +2,14 @@ package Day1;
 
 public class Calculator {
     public static void main(String[] args) {
+        int sum = 0;
 
+        // 1부터 100까지 더하기
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+
+        // 결과 출력
+        System.out.println("1부터 100까지의 합: " + sum + "입니다.");
     }
-}
+    }
