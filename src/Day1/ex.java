821cbcd62b35838e294418c310ec8d63f92eb305
minhyeokshin <sinminheyok@gmail.com>
2025-01-08 19:06:44 +0900

    Day1

--- src/Day1/ex.java
+++ src/Day1/ex.java
@@ -2,6 +2,10 @@ package Day1;
 
 public class ex {
     public static void main(String[] args) {
-
+        System.out.println("첫 번째 프로그램입니다.");
+        System.out.println("두 번째 프로그램입니다. 한 줄에 여러 문장을 출력합니다.");
+        System.out.println("세 번째 프로그램입니다.\n" + "줄을 바꾸어 출력합니다.\n" + "여기까지 출력하고\n" + "줄을 바꿉니다.");
+        System.out.println("서식 문자로 출력하는 방법입니다.\n" +
+                "이것은 문자열을 나타냅니다.");
     }
 }
