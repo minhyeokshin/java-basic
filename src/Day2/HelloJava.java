94f160f60188e5d4c52ba9453e8e153497e3160a
minhyeokshin <sinminheyok@gmail.com>
2025-01-09 12:00:57 +0900

    Day2

---
+++ src/Day2/HelloJava.java
@@ -0,0 +1,23 @@
+package Day2;
+
+import java.util.Scanner;
+
+/*
+1."Hello Java"문자열을 출력하는 HelloJava 클래스를 작성하세요.
+2."Hello Java"를 출력한 후 사용자에게 일므을 입력받아 "Hello 000"출력하세요
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java"); // Hello Java = Parameter
+        System.out.println("당신의 이름을 입력해 주세요.");
+        Scanner input = new Scanner(System.in);
+        /*Scanner Class는 Command+Enter로 입력
+        input = instance veriable
+        System.in -> Keyboard에서 입력된 Data
+         */
+        String name = input.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
