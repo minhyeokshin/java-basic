cf991063d15cc80daa86636ba2afe7f6caa7d2f3
minhyeokshin <sinminheyok@gmail.com>
2025-01-09 16:13:51 +0900

    Day2

---
+++ src/Day2/LiterelEx2.java
@@ -0,0 +1,16 @@
+package Day2;
+
+public class LiterelEx2 {
+    public static void main(String[] args) {
+
+        int var = 0b1011;
+        int var2 = 0206;
+        int var3 = 365;
+        int var4 = 0xB3;
+
+        System.out.println(var);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+    }
+}
