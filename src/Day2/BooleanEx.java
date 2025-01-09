a5eea39083681a5dfee5ba39e7bb1e976192af66
minhyeokshin <sinminheyok@gmail.com>
2025-01-09 16:55:39 +0900

    Day2

--- src/Day2/BooleanEx.java
+++ src/Day2/BooleanEx.java
@@ -6,6 +6,13 @@ public class BooleanEx {
         //두 가지 상태값을 저장할 필요가 있을 경우, 상태값에 따라 조건문과 제어문의 실행 흐름을 변경하는데 사용
         //연산식 중 비교, 논리 연산의 산출값을 판단할 때 사용
         boolean stop = true;
-        boolean start = false;
+        //boolean start = false;
+        if(stop) {
+            System.out.println("멈추세요!");
+        }else{
+            System.out.println("계속진행하세요");
+        }
+
+        while(true){}
     }
 }
