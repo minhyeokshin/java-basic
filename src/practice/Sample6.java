97aa180da46ba3bdc2117e6782259cff70573713
minhyeokshin <sinminheyok@gmail.com>
2025-01-12 02:42:27 +0900

    Day3

---
+++ src/practice/Sample6.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Sample6 {
+    public static void main(String[] args) {
+        System.out.println("10진수 10은 " + 10 + "입니다.");
+        System.out.println("8진수 10은 " + 010 + "입니다.");
+        System.out.println("16진수 10은 " + 0x10 + "입니다.");
+        System.out.println("16진수 F는 " + 0xF + "입니다.");
+    }
+}
